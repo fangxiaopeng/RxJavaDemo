94bb18f0312bfafc6fe9ee9126d911fba307195a
fangxiaopeng <fxp850899969>
2018-06-22 16:22:27 +0800

    update

--- RxJavaDemo/app/src/main/java/com/example/rxjavademo/activity/MainActivity.java
+++ RxJavaDemo/app/src/main/java/com/example/rxjavademo/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.fxp.rxjavademo;
+package com.example.fxp.rxjavademo.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.fxp.rxjavademo.R;
 
 public class MainActivity extends AppCompatActivity {
 
